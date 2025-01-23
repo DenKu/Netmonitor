ab15557b21497cd134ac57ee71fbbb2b9835e6b3
DenKu <DenKu@users.noreply.github.com>
2025-01-23 11:19:40 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<title>Network Monitor</title>
 	<link rel="stylesheet" type="text/css" href="netmonitor.css" media="screen" />
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.2.1/chart.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <body>
 <h1 class="headline">Network Monitor</h1>
